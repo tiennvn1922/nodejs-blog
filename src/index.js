8c7b73082abcf3ea7d15a265502969b8b4087c65
tiennvn1922 <tiennvn1922@gmail.com>
2022-10-24 10:25:41 +0700

    install prettier, lint-staged, husky

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ const app = express();
 const port = 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
-                                app.use(
+app.use(
     express.urlencoded({
         extended: true,
     }),
